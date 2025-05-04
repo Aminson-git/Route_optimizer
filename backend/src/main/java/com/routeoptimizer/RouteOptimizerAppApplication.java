fea3873ad563d86ef9b9cd13a482cda73a688a36
Aminson-git <moktanaminson@gmail.com>
2025-05-04 22:59:15 +0100

    Initial push of the optimizer

---
+++ backend/src/main/java/com/routeoptimizer/RouteOptimizerAppApplication.java
@@ -0,0 +1,13 @@
+package com.routeoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteOptimizerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteOptimizerAppApplication.class, args);
+	}
+
+}
